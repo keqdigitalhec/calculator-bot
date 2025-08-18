b9fc5f9b8c89a7023501041e4cef8e5695d99f6c
akrash <133720072+keqdigitalhec@users.noreply.github.com>
2025-08-18 22:40:53 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.enums import ParseMode
+from aiogram.filters import Command
+from aiogram.types import Message
+
+TOKEN = "YOUR BOT TOKEN"
+
+from aiogram.client.default import DefaultBotProperties
+
+bot = Bot(
+    token=TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: Message):
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(
+            text="🧮 Калькулятор дробів",
+            web_app=WebAppInfo(url="YOUR WEBAPP URL")
+        )]
+    ])
+    await message.answer("Натисни кнопку, щоб запустити калькулятор:", reply_markup=keyboard)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
